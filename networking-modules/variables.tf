7a6128d9fe177bda6ac0b85ae934a032df699943
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2024-01-19 23:01:08 +0000

    Commented out values for variables

--- networking-modules/variables.tf
+++ networking-modules/variables.tf
@@ -1,18 +1,18 @@
 variable "resource_group_name" {
     description = "Name of resource group."
     type = string
-    default = "Networking-Resource-Group"
+    # default = "Networking-Resource-Group"
 }
 
 variable "resource_location" {
     description = "location of resource group"
     type = string
-    default = "UK South"  
+    # default = "UK South"  
 }
 
 variable "vnet_address_space" {
     description = "Address space for the virtual network"
     type = list(string)
-    default = [ "10.0.0.0/16" ]
+    # default = [ "10.0.0.0/16" ]
   
 }
